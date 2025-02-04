3cecb3add3452cca0a93699f369af5102ebd3d3d
toli1987 <toli1987@protonmail.com>
2025-02-04 22:54:13 +0100

    Start form!

--- src/pages/template/new.jsx
+++ src/pages/template/new.jsx
@@ -1,3 +1,4 @@
+import { Card, Grid2, Input, TextField } from "@mui/material";
 import { useEffect, useState } from "react";
 
 const defaultValues = {
@@ -36,7 +37,88 @@ function NewTemplate() {
   return (
     <div>
       New Template form!
-      <form onSubmit={handleSubmit}></form>
+      <form onSubmit={handleSubmit}>
+        <Grid2
+          container
+          direction='row'
+          sx={{ alignItems: "center", justifyContent: "center" }}
+        >
+          <Grid2
+            size={6}
+            rowSpacing={5}
+            rowGap={10}
+          >
+            <Grid2 size={6}>
+              <TextField
+                label={"Name"}
+                required
+                id='templateName'
+                name='Name'
+                variant='outlined'
+                helperText='Name of Template'
+              />
+            </Grid2>
+
+            <Grid2 size={6}>
+              <TextField
+                label={"Description"}
+                required
+                id='templateDescription'
+                name='Description'
+                variant='outlined'
+                helperText='Description of Template'
+              />
+            </Grid2>
+            <Grid2 size={6}>
+              <TextField
+                label={"Category"}
+                required
+                id='templateCategory'
+                name='Category'
+                variant='outlined'
+                helperText='Category of Template'
+              />
+            </Grid2>
+          </Grid2>
+          <Grid2
+            size={6}
+            rowSpacing={1}
+          >
+            <Card>
+              <Grid2 size={6}>
+                <TextField
+                  label={"Name"}
+                  required
+                  id='templateName'
+                  name='Name'
+                  variant='outlined'
+                  helperText='Name of Template'
+                />
+              </Grid2>
+              <Grid2 size={6}>
+                <TextField
+                  label={"Description"}
+                  required
+                  id='templateDescription'
+                  name='Description'
+                  variant='outlined'
+                  helperText='Description of Template'
+                />
+              </Grid2>
+              <Grid2 size={6}>
+                <TextField
+                  label={"Category"}
+                  required
+                  id='templateCategory'
+                  name='Category'
+                  variant='outlined'
+                  helperText='Category of Template'
+                />
+              </Grid2>
+            </Card>
+          </Grid2>
+        </Grid2>
+      </form>
     </div>
   );
 }
